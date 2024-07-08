161e47127ccc1f18fed4ab64548d8de93cdd817f
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-08 17:46:48 -0400

    Agregando Dtos de los modelos

---
+++ UserBlazorApp.API/Dtos/Roles/RolRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace UserBlazorApp.API.Dtos.Roles;
+using UserBlazorApp.API.Dtos.Claims;
+
+public class RolRequest
+{
+	public int RoleId { get; set; }
+	public string? Name { get; set; }
+
+	public List<RolClaimRequest> Claims { get; set; } = new List<RolClaimRequest>();
+}
---
+++ UserBlazorApp.API/Dtos/User/UserRequest.cs
@@ -0,0 +1,16 @@
+﻿using UserBlazorApp.API.Dtos.Claims;
+using UserBlazorApp.API.Dtos.Roles;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.API.Dtos.User;
+
+public class UserRequest
+{
+    //public int Id { get; set; }
+    public string? UserName { get; set; }
+    public string? Email { get; set; }
+    public string? PasswordHash { get; set; }
+    public string? PhoneNumber { get; set; }
+    public DateTimeOffset? LockoutEnd { get; set; }
+	public ICollection<RolRequest> Role { get; set; } = new List<RolRequest>();
+}
