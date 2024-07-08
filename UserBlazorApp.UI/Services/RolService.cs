e33b8a941d857687d4768e8b6675f95b3e238945
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-08 17:51:10 -0400

    Agregando servicios de los modelos en donde consumen las apis

---
+++ UserBlazorApp.UI/Services/RolService.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+
+public class RolService(HttpClient httpClient) : IClientService<AspNetRoles>
+{
+	public async Task<List<AspNetRoles>> GetAllAsync()
+	{
+		return await httpClient.GetFromJsonAsync<List<AspNetRoles>>("api/AspNetRoles");
+	}
+	public async Task<AspNetRoles> GetByIdAsync(int id)
+	{
+		return await httpClient.GetFromJsonAsync<AspNetRoles>($"api/AspNetRoles/{id}");
+	}
+	public async Task<AspNetRoles> AddAsync(AspNetRoles entity)
+	{
+		var response = await httpClient.PostAsJsonAsync("api/AspNetRoles", entity);
+		return await response.Content.ReadFromJsonAsync<AspNetRoles>();
+	}
+
+	public async Task<AspNetRoleClaims> AddClaimToRoleAsync(int roleId, AspNetRoleClaims claimRequest)
+	{
+		var response = await httpClient.PostAsJsonAsync($"api/AspNetRoles/{roleId}/claims", claimRequest);
+		response.EnsureSuccessStatusCode();
+		return await response.Content.ReadFromJsonAsync<AspNetRoleClaims>();
+	}
+	public async Task<bool> UpdateAsync(AspNetRoles entity)
+	{
+		var response = await httpClient.PutAsJsonAsync($"api/AspNetRoles/{entity.Id}", entity);
+		return response.IsSuccessStatusCode;
+	}
+
+	public async Task<bool> DeleteAsync(int id)
+	{
+		var response = await httpClient.DeleteAsync($"api/AspNetRoles/{id}");
+		return response.IsSuccessStatusCode;
+	}
+
+	public async Task<bool> DeleteRoleClaimAsync(int roleId, int claimId)
+	{
+		var response = await httpClient.DeleteAsync($"api/AspNetRoles/{roleId}/claims/{claimId}");
+		return response.IsSuccessStatusCode;
+	}
+
+
+
+}
---
+++ UserBlazorApp.UI/Services/UsuarioService.cs
@@ -0,0 +1,46 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using UsersBlazorApp.Data.Interfaces;
+using UsersBlazorApp.Data.Models;
+
+namespace UserBlazorApp.UI.Services;
+
+public class UsuarioService(HttpClient httpClient) : IClientService<AspNetUsers>
+{
+	public async Task<List<AspNetUsers>> GetAllAsync()
+	{
+		return await httpClient.GetFromJsonAsync<List<AspNetUsers>>("api/AspNetUsers");
+	}
+
+	public async Task<AspNetUsers> GetByIdAsync(int id)
+	{
+		return (await httpClient.GetFromJsonAsync<AspNetUsers>($"api/AspNetUsers/{id}"))!;
+	}
+
+	public async Task<AspNetUsers> AddAsync(AspNetUsers entity)
+	{
+        var response = await httpClient.PostAsJsonAsync("api/AspNetUsers", entity);
+
+        if (response.IsSuccessStatusCode)
+        {
+            return await response.Content.ReadFromJsonAsync<AspNetUsers>();
+        }
+        else
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            Console.Error.WriteLine($"Error: {errorContent}");
+            return null;
+        }
+    }
+	public async Task<bool> UpdateAsync(AspNetUsers entity)
+	{
+		var response = await httpClient.PutAsJsonAsync($"api/AspNetUsers/{entity.Id}", entity);
+		return response.IsSuccessStatusCode;
+	}
+
+	public async Task<bool> DeleteAsync(int id)
+	{
+		var response = await httpClient.DeleteAsync($"api/AspNetUsers/{id}");
+		return response.IsSuccessStatusCode;
+	}
+}
